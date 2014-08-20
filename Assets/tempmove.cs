65f477dbce55088883382d1f20c664bb34557a8b
shivashankar <shivashankar.achar@gmail.com>
2014-08-20 14:15:25 +0530

    Update tempmove.cs
    madd the same to  tempscript

--- Assets/tempmove.cs
+++ Assets/tempmove.cs
@@ -17,6 +17,12 @@ public class tempmove : MonoBehaviour {
 	            position.y--;
 	            this.transform.position = position;
 	        }
+	        	if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+		}
 
 	}
 }
