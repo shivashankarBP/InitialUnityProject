bb578aba43734885b873a660fa6281e6d96f3dd3
shivashankar <shivashankar.achar@gmail.com>
2014-08-20 14:22:30 +0530

    Update CubeHandler.cs
    newchange

--- Assets/CubeHandler.cs
+++ Assets/CubeHandler.cs
@@ -33,5 +33,33 @@ public class CubeHandler : MonoBehaviour {
 			position.x++;
 			this.transform.position = position;
 		}
+		
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y++;
+			this.transform.position = position;
+		}
+
+		//let me add few more n check if it works in all.
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;
+		}
+		
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y--;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+		}
 	}
 }
