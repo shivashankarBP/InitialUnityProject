744cdb8ed5c2b96695aebef6fae68266ae938c64
shivashankarBP <subaja@GD23-iMac.local>
2014-08-20 14:05:47 +0530

    move with new script

---
+++ Assets/tempmove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tempmove : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y++;
+			this.transform.position = position;
+		}
+
+	}
+}
